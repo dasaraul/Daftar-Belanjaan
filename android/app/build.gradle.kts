788ea309176871d4bd705747c235d8972f815e45
Dasaraul <tamskun29@gmail.com>
2025-05-06 19:33:15 -0700

    fix package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.daftar_belanjaan_new"
+    namespace = "com.abdulrahman.daftar_belanjaan"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.daftar_belanjaan_new"
+        applicationId = "com.abdulrahman.daftar_belanjaan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
